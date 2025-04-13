8338b13345265e3fef5912319f14034c49e6624c
Trubber <73858351+Trubber@users.noreply.github.com>
2025-04-13 11:50:20 -0400

    updated model

--- deeplearningmodel.py
+++ deeplearningmodel.py
@@ -5,26 +5,29 @@ import keras
 from keras import layers, utils
 import numpy as np
 import pickle
+import pyshark
 
 
 load_dotenv()
 
-with open(os.getenv("UNDEFENDED_X_TE"), 'rb') as file:
+
+
+with open(os.getenv("W-T-SIM_X_TE"), 'rb') as file:
     X_te = pickle.load(file)
 
-with open(os.getenv("UNDEFENDED_X_TR"), 'rb') as file:
+with open(os.getenv("W-T-SIM_X_TR"), 'rb') as file:
     X_tr = pickle.load(file)
 
-with open(os.getenv("UNDEFENDED_X_VL"), 'rb') as file:
+with open(os.getenv("W-T-SIM_X_VL"), 'rb') as file:
     X_vl = pickle.load(file)
 
-with open(os.getenv("UNDEFENDED_Y_TE"), 'rb') as file:
+with open(os.getenv("W-T-SIM_Y_TE"), 'rb') as file:
     Y_te = pickle.load(file)
 
-with open(os.getenv("UNDEFENDED_Y_TR"), 'rb') as file:
+with open(os.getenv("W-T-SIM_Y_TR"), 'rb') as file:
     Y_tr = pickle.load(file)
 
-with open(os.getenv("UNDEFENDED_Y_VL"), 'rb') as file:
+with open(os.getenv("W-T-SIM_Y_VL"), 'rb') as file:
     Y_vl = pickle.load(file)
 
 X_train = np.array(X_tr)
@@ -41,40 +44,53 @@ Y_valid = utils.to_categorical(y_valid, num_classes=num_classes)
 Y_test = utils.to_categorical(y_test, num_classes=num_classes)
 
 
+
+
+model = keras.models.Sequential()
+
+
 #This model WIP, basic one that can run the tik_tok datasets
-model = keras.Sequential()
+model.add(layers.Input((160, 1)))
 
-model.add(layers.Input((160,1)))
-
-model.add(layers.Conv1D(filters=32, kernel_size=16, strides=1))
-model.add(layers.BatchNormalization(axis=-1))
+# First Convolutional Block
+model.add(layers.Conv1D(filters=8, kernel_size=8, strides=1))
+model.add(layers.BatchNormalization())
 model.add(layers.ELU())
 model.add(layers.MaxPooling1D())
 
+# Second Convolutional Block
 model.add(layers.Conv1D(filters=16, kernel_size=8, strides=1))
 model.add(layers.BatchNormalization())
 model.add(layers.ELU())
 model.add(layers.MaxPooling1D())
 
-model.add(layers.Conv1D(filters=16, kernel_size=8, strides=1))
+# Third Convolutional Block
+model.add(layers.Conv1D(filters=32, kernel_size=4, strides=1))
 model.add(layers.BatchNormalization())
 model.add(layers.ELU())
 model.add(layers.MaxPooling1D())
 
-model.add(layers.Flatten())
+# Global Average Pooling
+model.add(layers.GlobalAveragePooling1D())
 
-model.add(layers.Dense(256, activation='relu'))
+# Fully Connected Layers
+model.add(layers.Dense(512, activation='relu'))
 model.add(layers.BatchNormalization())
 model.add(layers.Dropout(0.3))
 
-model.add(layers.Dense(num_classes, activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.Dropout(0.65))
 
-model.add(layers.Dense(num_classes, activation='relu'))
+model.add(layers.Dense(512, activation='relu'))
 model.add(layers.BatchNormalization())
-model.add(layers.Dropout(0.1))
-model.add(layers.Activation('softmax', name="softmax"))
+model.add(layers.Dropout(0.4))
+
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.Dropout(0.3))
+# Output Layer
+model.add(layers.Dense(num_classes, activation='softmax'))
+
+# Summary of the model
+model.summary()
 
 
 #compilation
@@ -83,7 +99,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 #training
-model.fit(X_train, Y_train, epochs=10, batch_size=128, validation_data=(X_valid, Y_valid))
+model.fit(X_train, Y_train, epochs=5, batch_size=2, validation_data=(X_valid, Y_valid))
 
 #evaluation
 #test_loss, test_acc = model.evaluate(X_test, Y_test, verbose=0)
