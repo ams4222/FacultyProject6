0b61661364401e26e77963d40f9100caea16e91e
Group 6 <cjf7669@rit.edu>
2025-05-03 12:45:27 -0400

    Final Client application

--- Client.py
+++ Client.py
@@ -2,53 +2,67 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
 import subprocess
-import time
+import signal
+import csv
+import random
 
-websites = [
-    "https://www.google.com",
-    "https://www.youtube.com",
-    "https://www.facebook.com",
-    "https://www.amazon.com",
-    "https://www.wikipedia.org",
-    "https://www.reddit.com",
-    "https://www.yahoo.com",
-    "https://www.instagram.com",
-    "https://www.linkedin.com",
-    "https://www.twitter.com"
-]
+
+websites = []
+with open('/app/domains.csv', mode='r', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        websites.append("https://" + row['Domain'])    
 
 # SOCKS5 Proxy Configuration
-SOCKS_PROXY_PORT = "" # Add proxy port when tunnel is running
+SOCKS_PROXY_PORT = "9050" 
 
 chrome_options = Options()
 # Set up the SOCKS5 proxy
-#chrome_options.add_argument('--proxy-server=socks5://127.0.0.1:' + SOCKS_PROXY_PORT)
+chrome_options.add_argument('--proxy-server=socks5://127.0.0.1:' + SOCKS_PROXY_PORT)
 
 # Add additional arguments
 chrome_options.page_load_strategy = 'normal'
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--edge-skip-compat-layer-relaunch')
 chrome_options.add_argument('--remote-debugging-port=9222')
 
 service = webdriver.ChromeService(log_output=subprocess.STDOUT)
 driver = webdriver.Chrome(service=service, options=chrome_options)
+driver.set_page_load_timeout(90)
 
-try:
-    #visit websites
-    for website in websites:
+index = 0
+for website in websites:
+    # Visit websites
+    try:
+        # Starts tcpdump capture
+        tcpdump = subprocess.Popen(["tcpdump", "-i", "any", "-w", "/app/pcap/traffic-" + str(index) + ".pcap"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        
+        # Visits website and prints website
         driver.get(website)
-        print(f"Visited: {website} - Title: {driver.title}")
+        print(f"Visited: {website} - Title: {driver.title}", flush=True)
+        
+        # Ends tcpdump capture
+        tcpdump.send_signal(signal.SIGINT)
+        tcpdump.wait()
+        
+        # Increases traffic file index
+        index += 1
 
-    # Play YouTube video
-    driver.get("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    video = driver.find_element(By.XPATH, "//*[@id=\"thumbnail\"]")
-    video = driver.find_element(By.ID, "movie_player")
-    video.send_keys(Keys.SPACE)
-    time.sleep(2)
-    video.click()
-    time.sleep(2)
-    print("Video played")
+    # Catches any webdriver or connection exceptions
+    except WebDriverException as e:
+        print("Exception occurred: ", e)
+        #if "Message: unknown error: net::ERR_SOCKS_CONNECTION_FAILED" in str(e):
+            #subprocess.run(["bash", "/app/restart_tor.sh"])
+    except Exception as e:
+        print("Exception occured: ", e)
 
-finally:
-    driver.quit()
+# Shutdown webdriver
+driver.quit()
+
+# Shutdown OpenVPN
+subprocess.Popen(['echo', 'signal', 'SIGTERM', '|nc', '127.0.0.1', '999'])
